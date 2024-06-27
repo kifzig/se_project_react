aeaa071dc89954fe359e39577da8041d7323472a
Kif_Francis <kif.zig@gmail.com>
2024-06-27 07:29:50 -0500

    Correct type in ProtectedRoute

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -3,7 +3,9 @@ import { Route, Redirect } from "react-router-dom";
 
 function ProtectedRoute({ children, loggedIn, ...props }) {
   return (
-    <Route {...props}>{loggedIn ? children : <Redirect to={"/sign"} />}</Route>
+    <Route {...props}>
+      {loggedIn ? children : <Redirect to={"/signin"} />}
+    </Route>
   );
 }
 
